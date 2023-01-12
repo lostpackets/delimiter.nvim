fdec798e25920995edb42eb0ab5769897f402e65
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-12 04:28:11 +0000

    Update main.sh

--- plugin/main.sh
+++ plugin/main.sh
@@ -1,26 +1,24 @@
-function! s:TestPy3() range
-    let currentLine = expand('<cword>')
+function! TestPy3() range
     let symbol = input("Enter the symbol to join bbw with: ")
     let save_pos = getpos('.')
+
+    if &selection == "visual"
+        let currentLine = expand('<cword>')
+    else
+        let currentLine = expand('<cword>')
+    endif
 
     python << EOF
 import vim
 import wordninja
 start=vim.eval('currentLine')
-
 bbw=wordninja.split(start)
-
 symbol = vim.eval('symbol')
 pr=symbol.join(bbw)
-
 EOF
     let X = pyeval('pr')
     execute "s/" .. currentLine .. "/" .. X .. "/"
     call setpos('.',save_pos)
-
 endfunction
-command! Test call s:TestPy3()
-
-nnoremap <F1> :call s:TestPy3()<CR>
-vnoremap <F1> :call s:TestPy3()<CR>
-
+nnoremap <F1> :call TestPy3()<CR>
+vnoremap <F1> :call TestPy3()<CR>
