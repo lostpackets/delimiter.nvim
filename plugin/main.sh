c349e2fa86403541a6bb5638a2dd41f47d24232c
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-12 02:58:03 +0000

    Create main.sh

---
+++ plugin/main.sh
@@ -0,0 +1,25 @@
+function! s:TestPy3() range
+    let currentLine = expand('<cword>')
+    let symbol = input("Enter the symbol to join bbw with: ")
+    let save_pos = getpos('.')
+
+    python << EOF
+import vim
+import wordninja
+start=vim.eval('currentLine')
+
+bbw=wordninja.split(start)
+
+symbol = vim.eval('symbol')
+pr=symbol.join(bbw)
+
+EOF
+    let X = pyeval('pr')
+    execute "s/" .. currentLine .. "/" .. X .. "/"
+    call setpos('.',save_pos)
+
+endfunction
+command! Test call s:TestPy3()
+
+nmap <silent> <v>P :Test<CR>
+" <v>P is the -1 for the Test command, can be changed to any desired keybind.
