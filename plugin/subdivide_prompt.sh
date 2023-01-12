1372f5b138742a894d7426b491e264ada66d5cd2
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-12 10:51:36 +0000

    Create subdivide_prompt.sh

---
+++ plugin/subdivide_prompt.sh
@@ -0,0 +1,26 @@
+function! TestPy3() range
+    let num_chars = input("Enter the number of characters to divide into sections: ")
+    let symbol = input("Enter the symbol to join bbw with: ")
+    let save_pos = getpos('.')
+
+    if &selection == "visual"
+        let currentLine = expand('<cword>')
+    else
+        let currentLine = expand('<cword>')
+    endif
+
+    python << EOF
+import vim
+import re
+num_chars = int(vim.eval('num_chars'))
+start = vim.eval('currentLine')
+bbw = re.findall(r'.{'+str(num_chars)+'}', start)
+symbol = vim.eval('symbol')
+pr=symbol.join(bbw)
+EOF
+    let X = pyeval('pr')
+    execute "s/" .. currentLine .. "/" .. X .. "/"
+    call setpos('.',save_pos)
+endfunction
+nnoremap <F11> :call TestPy3()<CR>
+vnoremap <F11> :call TestPy3()<CR>
